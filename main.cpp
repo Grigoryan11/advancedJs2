4a9237994e6baad858690b088f5f560c584419f7
Suren <suren.karapetyan@ontid.com>
2022-07-14 17:22:26 +0400

    add strmove function

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 using namespace std;
 
+
 int strlen(char* str) {
 	int len = 0;
 
@@ -23,17 +24,37 @@ char* concat(char* str1,char* str2){
 	return str1;
 }
 
-char* strset(char* str,char *ch){
+char* strset(char* str,char ch){
 	for(int i = 0; i < strlen(str); i++) {
-		str[i] = *ch;
+		str[i] = ch;
 	}
 	return str;
+}
+
+char* strmove(char* str, int n){
+	int x = strlen(str);
+	int l;
+	if(n < 0){
+		return str;
+	} else {
+		for(int j = 0; j < n; j++) {
+			l = str[x - 1];
+ 			for(int i = x - 1; i > 0; i--) {
+				str[i] = str[i - 1];
+			}
+   			str[0] = l;	
+		}
+		return str;
+	}
 }
 
 int main(){
 	char ptr1[] = "hello";
 	char ptr2[] = "world";
-	char ch[] = "*";
-	//cout << concat(ptr1,ptr2) << endl;
-	//cout << strset(ptr1,ch) << endl;
+	char ch = '*';
+	char ptr3[] = "hello";
+	int num = -2;
+//	cout << concat(ptr1,ptr2) << endl;
+//	cout << strset(ptr1,ch) << endl;
+	cout << strmove(ptr3,num) << endl;
 }
